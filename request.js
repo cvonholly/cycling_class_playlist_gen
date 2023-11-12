1d01fbc7cd17ec4b84a51eefc87daf8e3e7f9ca3
Carl von Holly <cvonholly@student.ethz.ch>
2023-11-12 14:51:40 +0100

    -- implement authorization code flow

---
+++ request.js
@@ -0,0 +1,19 @@
+// var client_id = '54cfccda4e9943e5a50fcbe7fe29b695';
+// var client_secret = '8742fe77513d42809fe3b6cb1acaa30a';
+
+// var authOptions = {
+//   url: 'https://accounts.spotify.com/api/token',
+//   headers: {
+//     'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+//   },
+//   form: {
+//     grant_type: 'client_credentials'
+//   },
+//   json: true
+// };
+
+// request.post(authOptions, function(error, response, body) {
+//   if (!error && response.statusCode === 200) {
+//     var token = body.access_token;
+//   }
+// });
